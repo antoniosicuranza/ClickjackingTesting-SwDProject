7e6de6dd404d9dc3578ad40a77a59c101389b7d1
antoniosicuranza <a.sicuranza5@studenti.unisa.it>
2021-04-24 20:57:14 +0200

    I created the first class to disable javascript technique

---
+++ ClickjackingTest/src/main/java/disablingJavascript.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+
+
+
+public class disablingJavascript {
+ 
+	public boolean disablingJavascriptRun() throws IOException {
+	return securityRestricted() && sandbox();
+	}
+	
+	public boolean securityRestricted() throws IOException {
+		String urltext = "http://localhost:8080/ClickjackingWebSource/disablingJavascriptTest.html";
+	    URL url = new URL(urltext);
+	    int responseCode = ((HttpURLConnection) url.openConnection()).getResponseCode();
+	    if(responseCode==200) {
+		return true;}
+	    else {
+	    	return false;
+	    }
+	}
+	public boolean sandbox() throws IOException {
+		String urltext = "http://localhost:8080/ClickjackingWebSource/disablingJavascriptTest2.html";
+	    URL url = new URL(urltext);
+	    int responseCode = ((HttpURLConnection) url.openConnection()).getResponseCode();
+	    if(responseCode==200) {
+		return true;}
+	    else {
+	    	return false;
+	    }
+	}
+}
---
+++ ClickjackingTest/src/main/java/mainClass.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class mainClass {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		disablingJavascript d=new disablingJavascript();
+		System.out.println(d.disablingJavascriptRun());
+	}
+
+}
