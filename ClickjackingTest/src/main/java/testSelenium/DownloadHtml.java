ca059a73101d88fecac29176ab893d0f97d282ac
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-04-26 21:36:09 +0200

    I wrote this code that permit to switch in the various iframe of a code and download the html of the various frames

---
+++ ClickjackingTest/src/main/java/testSelenium/DownloadHtml.java
@@ -0,0 +1,28 @@
+package testSelenium;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class DownloadHtml {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/ClickjackingTest/disablingJavascriptTest2.html");
+		//int size = driver.findElements(By.tagName("iframe")).size();
+		driver.switchTo().frame(driver.findElementByTagName("iframe"));
+		driver.switchTo().frame(driver.findElementByTagName("iframe"));
+		System.out.println(driver.getPageSource());
+	}
+
+}
