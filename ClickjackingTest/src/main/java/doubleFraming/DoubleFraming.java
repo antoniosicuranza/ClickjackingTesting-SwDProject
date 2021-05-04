86f0cc8a967386877b412fda6e418af3c6e42b12
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-04 23:24:13 +0200

    improvements of the method for detecting if a site is vulnerable or not from a clickjacking, in particular this method is based on the fact that a class that has a protection on clickjacking doesn't load style tag of the css in the page, and if it to do this launch an exception that the tag wasn't found

--- ClickjackingTest/src/main/java/doubleFraming/DoubleFraming.java
+++ ClickjackingTest/src/main/java/doubleFraming/DoubleFraming.java
@@ -1,8 +1,10 @@
 package doubleFraming;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DoubleFraming {
 	public boolean doubleCalling() throws IOException {
@@ -10,14 +12,18 @@ public class DoubleFraming {
 	}
 	
 	public boolean calling() throws IOException {
-		String urlTxt = "http://localhost:8080/doubleFraming/fictitious2.html";
-		URL url = new URL(urlTxt);
-		int responseCode = ((HttpURLConnection) url.openConnection()).getResponseCode();
-		System.out.println("ResponseCode: "+responseCode);
-		if(responseCode==200)
-			return true;
-		else
-			return false;
+		boolean result = true;
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/ClickjackingTest/fictitious2.html");
+		driver.switchTo().frame(driver.findElementByTagName("iframe"));
+		try {
+			 driver.findElementByTagName("style");
+		}catch (NoSuchElementException exception) {
+			result = false;
+		}
+		System.out.println("Restricted: "+result);
+	    driver.quit();
+	    return result ;
 		
 	}
 }
