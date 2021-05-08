bcdbf20d0996c4a44a5f60c723649768318eb5ff
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-08 20:24:36 +0200

    feat: I have have implemented a servlet ReferenceClickjackingSites
    Description: in this servlet I set the parameters of the clickjacking jsp file in particular the src fields in a dynamic way, trough this servlet I can maintain more decoupling

---
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/controller/ReferenceClickjackingSites.java
@@ -0,0 +1,48 @@
+package com.dependability.clickjacking.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dependability.clickjacking.reference.References;
+
+/**
+ * Servlet implementation class ReferenceClickjackingSites
+ */
+@WebServlet("/ReferenceClickjackingSites")
+public class ReferenceClickjackingSites extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReferenceClickjackingSites() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		References ref = new References ("http://localhost:3000/",5);
+		ref.selectAttack();
+		request.setAttribute("link", ref.getSrc());
+		request.getRequestDispatcher(ref.getPage()).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
