12d94f246dca0542a8da31695664c4c5c125d1a6
antoniosicuranza <a.sicuranza5@studenti.unisa.it>
2021-05-12 18:31:38 +0200

    Servlet connection
    The servlet is connected to the interface to take the url to test and
    the test technique that the user want to perform

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/controller/ReferenceClickjackingSites.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/controller/ReferenceClickjackingSites.java
@@ -15,7 +15,8 @@ import com.dependability.clickjacking.reference.References;
 @WebServlet("/ReferenceClickjackingSites")
 public class ReferenceClickjackingSites extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	public String url;
+    public boolean[] check= {false,false,false,false,false}; 
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,7 +31,17 @@ public class ReferenceClickjackingSites extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		References ref = new References ("http://localhost:3000/",5);
+		//References ref = new References (url,1);
+		//ref.selectAttack();
+		//request.setAttribute("link", ref.getSrc());
+		//request.getRequestDispatcher(ref.getPage()).forward(request, response);
+		url=request.getParameter("url");
+		System.out.println("url:"+url);
+		for(int i = 0; i<5; i++) {
+			check[i]=request.getParameter(""+i) != null;
+		}
+		
+		References ref = new References (url,1);
 		ref.selectAttack();
 		request.setAttribute("link", ref.getSrc());
 		request.getRequestDispatcher(ref.getPage()).forward(request, response);
@@ -42,7 +53,8 @@ public class ReferenceClickjackingSites extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+				doGet(request,response);
 	}
+	
 
 }
