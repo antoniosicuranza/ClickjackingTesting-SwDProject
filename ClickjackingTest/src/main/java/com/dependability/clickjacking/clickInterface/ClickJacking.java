218bc58c842385a1487ea850d04c80a0398e8e63
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-13 22:50:05 +0200

    modification: Modification applied to the interface
    description: I have added the get and set method to manage htmlFile

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/clickInterface/ClickJacking.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/clickInterface/ClickJacking.java
@@ -1,11 +1,15 @@
 package com.dependability.clickjacking.clickInterface;
 
+import java.io.File;
 import java.io.IOException;
 
 public interface ClickJacking {
 	String src = "";
+	File f = null;
 	public void creation()  throws IOException ;
 	public void run()  throws IOException ;
 	public String getSrc();
 	public void setSrc(String src);
+	public File getHtmlFile();
+	public void setHtmlFile(File f);
 }
