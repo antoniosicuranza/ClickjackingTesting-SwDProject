b4331325f95ffb962e233db0c71c82745f25bcbb
antoniosicuranza <a.sicuranza5@studenti.unisa.it>
2021-05-14 12:05:38 +0200

    Error correction

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/disablingJavascript/DisablingJavascriptChrome.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/disablingJavascript/DisablingJavascriptChrome.java
@@ -1,5 +1,57 @@
 package com.dependability.clickjacking.creation.disablingJavascript;
 
-public class DisablingJavascriptChrome {
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+import com.dependability.clickjacking.clickInterface.ClickJacking;
+
+public class DisablingJavascriptChrome implements ClickJacking{
+
+	private String src;
+	private File f;
+	
+	public DisablingJavascriptChrome(String src){
+		this.src = src;
+		f = new File("src\\main\\webapp\\disablingJavascript.html");
+	}
+	
+	@Override
+	public String getSrc() {
+		return src;
+	}
+	
+	@Override
+	public void setSrc(String src) {
+		this.src = src;
+		
+	}
+	
+	@Override
+	public File getHtmlFile() {
+		return f;
+	}
+	
+	@Override
+	public void setHtmlFile(File f) {
+		this.f = f;
+	}
+	
+	
+	@Override
+	public void creation() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write("<html><body>");
+        bw.write("<iframe src="+src+" sandbox></iframe>");
+        bw.write("</body></html>");
+        bw.close();		
+	}
+	
+	@Override
+	public void run() throws IOException {
+		Desktop.getDesktop().browse(f.toURI());
+	}
 }
+
