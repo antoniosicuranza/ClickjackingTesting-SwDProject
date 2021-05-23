0c21c983bae38caf9f1a38762ad2f2d5a83543b3
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-23 22:15:27 +0200

    refactoring: Decoupling assignment of path
    description: Through the class FileCustom described in the commit "c3e8949ef922487bf9a474d05ca9a5f389fffcbe" we don't have to anymore change the path in the line 19 if we change the execution path of the project.

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/onBeforeUnloadEvent/OnBeforeUnloadEvent.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/onBeforeUnloadEvent/OnBeforeUnloadEvent.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import com.dependability.clickjacking.clickInterface.ClickJacking;
+import com.dependability.clickjacking.file.FileCustom;
 
 public class OnBeforeUnloadEvent implements ClickJacking{
 
@@ -15,7 +16,8 @@ public class OnBeforeUnloadEvent implements ClickJacking{
 	
 	public OnBeforeUnloadEvent(String src){
 		this.src = src;
-		f = new File("src\\main\\webapp\\html_generated\\onBeforeUnloadEvent.html");
+	    f = (new FileCustom("src\\main\\webapp\\html_generated\\onBeforeUnloadEvent.html")).getFile();
+
 	}
 	
 	@Override
