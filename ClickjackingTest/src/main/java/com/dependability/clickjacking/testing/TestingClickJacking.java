53afb6dc26ffa9f76f3a36950ca42e8ad8011650
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-24 14:03:35 +0200

    improvement: improve wait of synchronization of server, read csv path
    description:
    Form line 130-138:
    i have improved the mechanism of wait of the deployment of the html file
    on server, indeed in the previously code at line 123 there was a wait
    statically of 10 seconds, to improve this mechanism in more smart way i
    have exploit the class TestPageOk() described in the commit:
    "8505dadf778fe6eac58334bd0edee21b4afbdfdd", indeed now as we can see in
    the line 131 in the file modified, we instantiate the class TestPage
    passing as parameters, the url on which is loaded the html page
    dynamically created, and the number of tentative, that we want to make
    to do, to see if the process of loading of the html page dynamically
    created give is all ok. So in this way when we execute at line 133 the
    method testPageExist() we make "n" tentative and each tentative wait
    seconds before to make another, and in each tentative the code try to
    load the page at the url passed, and if one of the n response is 202, is
    all ok, if after the n tentative there is a response different from 202
    there is the catching of exception and so we have the print of the HTTP
    response code to understand what is the issue. Another important
    improvement of this mechanism is that before, the wait mechanism with
    Thread.sleep() for the synchronization with the deployment of the file
    pn the server, is applied directly in this class now all the mechanism
    is made in the TestPageOk so this increase the decoupling of
    responsibility increasing the maintainability of the code, and the reuse
    of the mechanism implemented in the class TestPageOKthis in other
    situation.
    
    At line 194: Through the class FileCustom described in the commit
    "c3e8949ef922487bf9a474d05ca9a5f389fffcbe" we don't have to anymore
    change the path in this line if we change the execution path of the
    project.

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/testing/TestingClickJacking.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/testing/TestingClickJacking.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.NoSuchElementException;
@@ -20,9 +22,12 @@ import com.dependability.clickjacking.creation.disablingJavascript.DisablingJava
 import com.dependability.clickjacking.creation.disablingJavascript.DisablingJavascriptIE;
 import com.dependability.clickjacking.creation.onBeforeUnloadEvent.OnBeforeUnloadEvent;
 import com.dependability.clickjacking.creation.redefiningLocation.RedefiningLocation;
+import com.dependability.clickjacking.file.FileCustom;
+import com.dependability.connection.TestPageOk;
+import com.dependability.exception.ErrorPage;
 
 public class TestingClickJacking {
-	
+
 	private String url;
 	private boolean[] listAttack;
 	private ClickJacking[] listClickJacking;
@@ -30,207 +35,228 @@ public class TestingClickJacking {
 	private boolean[] results;
 	private static int chrome = 0;
 	private static int internet_explorer = 1;
+
 	public TestingClickJacking(String url, boolean[] listAttack) {
 		this.listAttack = listAttack;
 		this.url = url;
 		listClickJacking = new ClickJacking[7];
 		browser = new int[7];
-		results= new boolean[7]; 
+		results = new boolean[7];
 	}
-	
+
 	public boolean[] getAttack() {
 		return listAttack;
 	}
-	
+
 	public void setAttack(boolean[] attack) {
 		this.listAttack = attack;
 	}
-	
+
 	public String getUrl() {
 		return this.url;
 	}
-	
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
 	public void creationEnvironment() throws IOException {
-		for(int i = 0; i < listAttack.length; i++) {
-			if(listAttack[i]) {
+		for (int i = 0; i < listAttack.length; i++) {
+			if (listAttack[i]) {
 				int idAttack = i;
 				selectAttack(idAttack);
 			}
 		}
-		
+
 	}
-	public void  selectAttack(int idAttack) throws IOException {
+
+	public void selectAttack(int idAttack) throws IOException {
 		ClickJacking click;
-		switch (idAttack){
-		    case 0://disablingJavascriptTest
-		    	click = new DisablingJavascriptChrome(url);
-		    	click.creation();
-		    	listClickJacking[0] = click;
-		    	browser[0] = chrome;
-		        break;
-		        
-		    case 1://disablingJavascriptTest2
-		    	click = new DisablingJavascriptIE(url);
-		    	click.creation();
-		    	listClickJacking[1] = click;
-		    	browser[1] = internet_explorer;
-		        break;
-		        
-		    case 2://doublFraming
-		    	click = new Fictitious2(url);
-		    	click.creation();
-		    	listClickJacking[2] = click;
-		    	browser[2] = chrome;
-		        break;
-		        
-		    case 3://filter_xss_chrome
-		    	click = new FilterXssChrome(url);
-		    	click.creation();
-		    	listClickJacking[3] = click;
-		    	browser[3] = chrome;
-		        break;
-		        
-		    case 4://filter_xss_IE8
-		    	click = new FilterXssIE(url);
-		    	click.creation();
-		    	listClickJacking[4] = click;
-		    	browser[4] = internet_explorer;
-		        break;
-		        
-		    case 5://onBeforeUnloadEventTest
-		    	click = new OnBeforeUnloadEvent(url);
-		    	click.creation();
-		    	listClickJacking[5] = click;
-		    	browser[5] = chrome;
-		        break;
-		        
-		    case 6://redefiningLocationIE
-		    	click = new RedefiningLocation(url);
-		    	click.creation();
-		    	listClickJacking[6] = click;
-		    	browser[6] = chrome;
-		        break;
-			}
-	}
-	
-	
-	private boolean test(ClickJacking clickJacking, int browser, int idAttack) throws IOException {// browser tra i parametri
-		try {
-			Thread.sleep(10000);//Wait  time for the deploy of the html generated on server
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		switch (idAttack) {
+		case 0:// disablingJavascriptTest
+			click = new DisablingJavascriptChrome(url);
+			click.creation();
+			listClickJacking[0] = click;
+			browser[0] = chrome;
+			break;
+
+		case 1:// disablingJavascriptTest2
+			click = new DisablingJavascriptIE(url);
+			click.creation();
+			listClickJacking[1] = click;
+			browser[1] = internet_explorer;
+			break;
+
+		case 2:// doublFraming
+			click = new Fictitious2(url);
+			click.creation();
+			listClickJacking[2] = click;
+			browser[2] = chrome;
+			break;
+
+		case 3:// filter_xss_chrome
+			click = new FilterXssChrome(url);
+			click.creation();
+			listClickJacking[3] = click;
+			browser[3] = chrome;
+			break;
+
+		case 4:// filter_xss_IE8
+			click = new FilterXssIE(url);
+			click.creation();
+			listClickJacking[4] = click;
+			browser[4] = internet_explorer;
+			break;
+
+		case 5:// onBeforeUnloadEventTest
+			click = new OnBeforeUnloadEvent(url);
+			click.creation();
+			listClickJacking[5] = click;
+			browser[5] = chrome;
+			break;
+
+		case 6:// redefiningLocationIE
+			click = new RedefiningLocation(url);
+			click.creation();
+			listClickJacking[6] = click;
+			browser[6] = chrome;
+			break;
 		}
+	}
+
+	private boolean test(ClickJacking clickJacking, int browser, int idAttack) throws IOException {
+		
 		boolean result = true;
-		String link = "http://localhost:8080/ClickjackingTest/html_generated/"+clickJacking.getHtmlFile().getName();
-        if(browser == 0) {
+		String link = "http://localhost:8080/ClickjackingTest/html_generated/" + clickJacking.getHtmlFile().getName();
+		TestPageOk testPage = new TestPageOk(link, 5);
+		try {
+			System.out.println(testPage.testPageExist());
+		} catch (ErrorPage e) {
+			System.out.println(e.getMessage());
+		} finally {
+			testPage.disconnect();
+		}
+		if (browser == 0) {
 			System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
 			ChromeDriver driverSwitchChrome = new ChromeDriver();
 			ChromeDriver driverChrome;
 			driverSwitchChrome.get(link);
 			try {
-				while(true)
+				while (true)
 					driverSwitchChrome.switchTo().frame(driverSwitchChrome.findElementByClassName("malicious"));
-			}
-			catch (NoSuchElementException exception) {
+			} catch (NoSuchElementException exception) {
 				driverChrome = driverSwitchChrome;
 			}
 			try {
-				//System.out.println(driverChrome.getPageSource());
+				// System.out.println(driverChrome.getPageSource());
 				driverChrome.findElementByTagName("style");
-			}
-			catch (NoSuchElementException exception2) {
+			} catch (NoSuchElementException exception2) {
 				result = false;
 			}
 			driverChrome.quit();
-		}
-		else if(browser == 1) {
+		} else if (browser == 1) {
 			System.setProperty("webdriver.ie.driver", "C:\\browserdrivers\\IEDriverServer.exe");
 			InternetExplorerDriver driverSwitchIE = new InternetExplorerDriver();
 			InternetExplorerDriver driverIE;
 			driverSwitchIE.get(link);
 
 			try {
-				while(true)
+				while (true)
 					driverSwitchIE.switchTo().frame(driverSwitchIE.findElementByClassName("malicious"));
-			}
-			catch (NoSuchElementException exception) {
+			} catch (NoSuchElementException exception) {
 				driverIE = driverSwitchIE;
 			}
 			try {
-				//System.out.println(driverIE.getPageSource());
+				// System.out.println(driverIE.getPageSource());
 				driverIE.findElementByTagName("style");
-			}
-			catch (NoSuchElementException | JavascriptException exception2) {
+			} catch (NoSuchElementException | JavascriptException exception2) {
 				result = false;
 			}
 			driverIE.quit();
 		}
-        System.out.println("IE: "+result);
+		System.out.println("result: " + result);
 		results[idAttack] = result;
-		return result;//true vulnerable, false not vulnerable
+		return result;// true vulnerable, false not vulnerable
 
 	}
-	
+
 	public String executionTest() throws IOException {
-		for(int i = 0; i < listAttack.length;i++) {
-			if(listAttack[i]) {
+		for (int i = 0; i < listAttack.length; i++) {
+			if (listAttack[i]) {
 				test(listClickJacking[i], browser[i], i);
 			}
 		}
-		return ""+(results[0]&&results[1]? "1":"0")+(results[2]? "1":"0")+(results[3]&&results[4]? "1":"0")+(results[5]? "1":"0")+(results[6]? "1":"0");
+		return "" + (results[0] && results[1] ? "1" : "0") + (results[2] ? "1" : "0")
+				+ (results[3] && results[4] ? "1" : "0") + (results[5] ? "1" : "0") + (results[6] ? "1" : "0");
 	}
-	
+
 	public String writeCSV() throws IOException {
-		String csv="C:\\Users\\sicur\\git\\ClickjackingTesting-SwDProject\\ClickjackingTest\\src\\main\\webapp\\csv\\test.csv";
-		FileWriter sb = new FileWriter(new File(csv));
+		String csv = "src\\main\\webapp\\csv\\test.csv";
+		FileWriter sb = new FileWriter(new FileCustom(csv).getFile());
 
-		      sb.append("DisablingJavascript");
-		      sb.append(',');
-		      sb.append("DoubleFraming");
-		      sb.append(',');
-		      sb.append("FilterXSS");
-		      sb.append(',');
-		      sb.append("onBeforeUnloadEvent");
-		      sb.append(',');
-		      sb.append("Redefininglocation");
-		      sb.append('\n');
+		sb.append("DisablingJavascript");
+		sb.append(',');
+		sb.append("DoubleFraming");
+		sb.append(',');
+		sb.append("FilterXSS");
+		sb.append(',');
+		sb.append("onBeforeUnloadEvent");
+		sb.append(',');
+		sb.append("Redefininglocation");
+		sb.append('\n');
 
-		      if(listAttack[0]) {sb.append((results[0]&&results[1]? "false":"true"));
-		      sb.append(',');}else {sb.append("null,");}
-		      if(listAttack[2]) {sb.append((results[2]? "false":"true"));
-		      sb.append(',');}else {sb.append("null,");}
-		      if(listAttack[3]) {sb.append((results[3]&&results[4]? "false":"true"));
-		      sb.append(',');}else {sb.append("null,");}
-		      if(listAttack[5]) {sb.append((results[5]? "false":"true"));
-		      sb.append(',');}else {sb.append("null,");}
-		      if(listAttack[6]) {sb.append((results[6]? "false":"true"));
-		      sb.append('\n');}else {sb.append("null\n");}
+		if (listAttack[0]) {
+			sb.append((results[0] && results[1] ? "false" : "true"));
+			sb.append(',');
+		} else {
+			sb.append("null,");
+		}
+		if (listAttack[2]) {
+			sb.append((results[2] ? "false" : "true"));
+			sb.append(',');
+		} else {
+			sb.append("null,");
+		}
+		if (listAttack[3]) {
+			sb.append((results[3] && results[4] ? "false" : "true"));
+			sb.append(',');
+		} else {
+			sb.append("null,");
+		}
+		if (listAttack[5]) {
+			sb.append((results[5] ? "false" : "true"));
+			sb.append(',');
+		} else {
+			sb.append("null,");
+		}
+		if (listAttack[6]) {
+			sb.append((results[6] ? "false" : "true"));
+			sb.append('\n');
+		} else {
+			sb.append("null\n");
+		}
 
-		      sb.flush();
-		      sb.close();
-		      
-		     return csv;
-		  	}
-	
+		sb.flush();
+		sb.close();
+
+		return csv;
+	}
+
 	public void cleanEnviroment() {
-		for(int i = 0; i < listAttack.length; i++) {
-			if(listAttack[i]) {
+		for (int i = 0; i < listAttack.length; i++) {
+			if (listAttack[i]) {
 				DeleteFile(listClickJacking[i].getHtmlFile().getPath());
 			}
-		} 
+		}
 	}
+
 	public void DeleteFile(String path) {
-		File f = new File(path); 
-	    if (f.delete()) { 
-	      System.out.println("Deleted the file: " + f.getName());
-	    } else {
-	      System.out.println("Failed to delete the file.");
-	    }
+		File f = new File(path);
+		if (f.delete()) {
+			System.out.println("Deleted the file: " + f.getName());
+		} else {
+			System.out.println("Failed to delete the file.");
+		}
 	}
-	
+
 }
