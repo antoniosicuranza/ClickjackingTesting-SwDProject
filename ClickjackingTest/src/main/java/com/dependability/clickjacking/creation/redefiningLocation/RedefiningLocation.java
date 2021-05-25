5c0148046f77ddcea3160d7674bbf1e7263f9047
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-25 21:03:59 +0200

    refactor: restyling and throws of IOException
    description:
    Line 12: throws the IOException by the constructor, because now the
    class "FileCustom" exploit file .properties so it throws in the
    constructor IOException, to more detail take a look the commit:
    "d1575da20be74696452533e5454d4d19e195825e"
    
    line 19: restyled the path assignment to the constructor new
    FileCustom(...)

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/redefiningLocation/RedefiningLocation.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/creation/redefiningLocation/RedefiningLocation.java
@@ -14,9 +14,9 @@ public class RedefiningLocation implements ClickJacking{
 	private String src;
 	private File f;
 	
-	public RedefiningLocation(String src){
+	public RedefiningLocation(String src) throws IOException{
 		this.src = src;
-	    f = (new FileCustom("src\\main\\webapp\\html_generated\\redefiningLocation.html")).getFile();
+	    f = (new FileCustom("src/main/webapp/html_generated/redefiningLocation.html")).getFile();
 
 	}
 	
