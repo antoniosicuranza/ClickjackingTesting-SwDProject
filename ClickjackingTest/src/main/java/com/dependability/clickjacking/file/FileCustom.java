136cdaf7bebe9b69e8e7c14c772b6de33cede0ad
JVALPASS <51170999+JVALPASS@users.noreply.github.com>
2021-05-26 13:30:37 +0200

    refactoring: Delegation manage of  properties to class ManageProperties
    description: decoupling of the managing of file config.properties, indeed before this was made in the line 13-14, now this responsibility is given to the class ManageProperties (described in the commit: "8be2d0e8023d72fe028435d2484e03f93fb9f60c").

--- ClickjackingTest/src/main/java/com/dependability/clickjacking/file/FileCustom.java
+++ ClickjackingTest/src/main/java/com/dependability/clickjacking/file/FileCustom.java
@@ -5,15 +5,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import com.dependability.clickjacking.properties.ManageProperties;
+
 public class FileCustom{
 	private File file;
-	private InputStream iS;
-	private Properties prop;
 	public FileCustom(String inProjectPath) throws IOException {
-		iS = this.getClass().getResourceAsStream("/config.properties");
-		prop = new Properties();
-		prop.load(iS);
-		String pathProject = prop.getProperty("path_project");
+		ManageProperties mP = new ManageProperties();
+		String pathProject = mP.retrievingProperty("path_project");
 		this.file = new File(pathProject + inProjectPath);
 	}
 	public File getFile(){
